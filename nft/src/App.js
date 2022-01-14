64b0568cca9866b268693a7457361fce78747cb8
Khandker <khandker.islam46@gmail.com>
2022-01-14 18:22:30 -0500

    finished component, might have to change size

--- nft/src/App.js
+++ nft/src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Card from './components/card';
+import {Card} from './components/card';
 
 function App() {
   return (
     <div className="App">
-      <Card title={"Equilibrium"} issue={3429}
+      <Card title={"Equilibrium"} issue={3429} description={"Our Equilibrium collection promotes balance and calm"} value={0.041} time={3} author={"Jules Wyvern"}/>
     </div>
   );
 }
--- nft/src/components/card.jsx
+++ nft/src/components/card.jsx
@@ -1,14 +1,43 @@
+import './card.css';
 
 export const Card = ({title, issue, description, value, time, creator, image}) => {
     return (
         <div className="single-card">
-            <h1>{title}</h1>
-            <p>{image}</p>
-            <p>{issue}</p>
-            <p>{description}</p>
-            <p>{value}</p>
-            <p>{time}</p>
-            <p>{creator}</p>
+            <article className="single-card__main-image">
+                <img 
+                    src="./assets/images/image-equilibrium.jpg" 
+                    alt=""
+                />
+                <div>
+                    <img src="./assets/images/icon-view.svg" alt="" />
+                </div>
+            </article>
+            <article>
+                <h2>{title}</h2>
+                <p>{description}</p>
+                <ul>
+                    <li>
+                        <img 
+                            src="./assets/images/icon-ethereum.svg" 
+                            alt=""
+                        />{`${value} ETH`}
+                    </li>
+                    <li>
+                        <img
+                            src="./assets/images/icon-clock.svg"
+                            alt=""
+                        />
+                        {`${time} days left`}
+                    </li>
+                </ul>
+            </article>
+            <article class="single-card__avatar">
+                <img 
+                    src="./assets/images/image-avatar.png" 
+                    alt=""
+                />
+                <p>Creation of <span>Jules Wyvern</span></p>
+            </article>
         </div>
     )
 }
